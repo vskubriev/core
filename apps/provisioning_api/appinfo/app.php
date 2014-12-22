461ee74e66a2fb80f68c4f440868a44e6c862fc6
Tom Needham <tom@owncloud.com>
2014-12-22 13:54:50 +0000

    Add provisioning_api app

---
+++ apps/provisioning_api/appinfo/app.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * ownCloud
+ *
+ * @copyright (C) 2014 ownCloud, Inc.
+ *
+ * @author Tom Needham <tom@owncloud.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+OC::$CLASSPATH['OCA\Provisioning_API\Users'] = 'provisioning_api/lib/users.php';
+OC::$CLASSPATH['OCA\Provisioning_API\Groups'] = 'provisioning_api/lib/groups.php';
+OC::$CLASSPATH['OCA\Provisioning_API\Apps'] = 'provisioning_api/lib/apps.php';
